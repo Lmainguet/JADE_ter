2f7fcfac55bc4bee5953ef18670bed8692615744
louane <louane.mainguet@gmail.com>
2026-05-07 19:37:17 +0200

    ajout du seed

--- code/Bert_model.py
+++ code/Bert_model.py
@@ -5,6 +5,21 @@ from torch.utils.data import DataLoader, TensorDataset
 from torch.optim import AdamW
 from sklearn.metrics import classification_report
 
+import random
+import numpy as np
+import torch
+
+def set_seed(seed=42):
+    random.seed(seed)
+    np.random.seed(seed)
+    torch.manual_seed(seed)
+    torch.cuda.manual_seed_all(seed)
+    torch.backends.cudnn.deterministic = True
+    torch.backends.cudnn.benchmark = False
+
+set_seed(42)
+
+
 # -------------------------
 # 1. Charger dataset
 # -------------------------
